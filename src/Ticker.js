de25390d92f472fd2e9aa2434dca542cb1b3baec
Hung Doan <19758667+hungdoansy@users.noreply.github.com>
2021-03-30 14:40:18 +0700

    fix(ticker): add layers to make it move smoothly

--- src/Ticker.js
+++ src/Ticker.js
@@ -3,10 +3,15 @@ import styled from "styled-components";
 const Ticker = ({ className }) => {
   return (
     <div className={className}>
-      <div className="ticker-wrapper">
-        <span className="ticker-text">
-          Super long text that needs to scroll
-        </span>
+      <div className="ticker-gradient-layer">
+        <div className="ticker-another-layer">
+          <div className="ticker-wrapper">
+            <span className="ticker-text">
+              Super long text that
+              {/* needs to scroll */}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -14,21 +19,13 @@ const Ticker = ({ className }) => {
 
 export default styled(Ticker)`
   position: fixed;
+  left: 200px;
   bottom: 0;
-  width: 100%;
+  width: 150px;
   overflow: hidden;
   height: 4rem;
-  background-color: transparent;
-  padding-left: 100%;
   box-sizing: content-box;
-
-  -webkit-mask-image: linear-gradient(
-    to right,
-    transparent,
-    #000 15%,
-    #000 85%,
-    transparent
-  );
+  background-color: transparent;
 
   @keyframes move {
     0% {
@@ -38,6 +35,29 @@ export default styled(Ticker)`
     100% {
       transform: translate3d(-100%, 0, 0);
     }
+  }
+
+  .ticker-gradient-layer {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+    background-color: transparent;
+
+    -webkit-mask-image: linear-gradient(
+      to right,
+      transparent,
+      #000 15%,
+      #000 85%,
+      transparent
+    );
+  }
+
+  .ticker-another-layer {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-left: 100%;
   }
 
   .ticker-wrapper {
